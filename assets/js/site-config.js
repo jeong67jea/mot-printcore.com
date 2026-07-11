b44784046d16632a7bc0adf0d5195540a867aae6
jeong67jea <jeongjae_choi@naver.com>
2026-07-11 20:16:45 +0900

    Update site-config.js

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -3,7 +3,8 @@ window.MOT_CONFIG = {
   brandName: "M.O.T. Technology Innovation Lab",
   siteUrl: "https://jeong67jea.github.io/mot-printcore.com/",
   contactEmail: "jeongjae_choi@naver.com",
-  phone: "+82 10 7502 0845",
+  phoneDisplay: "+82 10 7502 0845",
+  phoneLink: "+821075020845",
   wechat: "choijjai",
   location: "Korea · China Technical Collaboration",
   projectSubjectPrefix: "[M.O.T. Project Inquiry]"

762886b05cd854b39ddc7285613733ebba8553cf
jeong67jea <jeongjae_choi@naver.com>
2026-07-11 20:19:07 +0900

    Add files via upload

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,11 +1,12 @@
-/* M.O.T. 공개 연락처 설정 */
+/*
+  Update these four values before publishing to GitHub Pages.
+  Do not enter confidential personal contact information unless it is intended for public display.
+*/
 window.MOT_CONFIG = {
-  brandName: "M.O.T. Technology Innovation Lab",
-  siteUrl: "https://jeong67jea.github.io/mot-printcore.com/",
+  siteName: "M.O.T. Technology Innovation Lab",
   contactEmail: "jeongjae_choi@naver.com",
   phoneDisplay: "+82 10 7502 0845",
   phoneLink: "+821075020845",
   wechat: "choijjai",
-  location: "Korea · China Technical Collaboration",
-  projectSubjectPrefix: "[M.O.T. Project Inquiry]"
+  siteUrl: "https://jeong67jea.github.io/mot-printcore.com/"
 };
